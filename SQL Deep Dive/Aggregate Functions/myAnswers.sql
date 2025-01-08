059823e82b51777fefe4030dc0a81222c0bf9b54
RojayIG <RojayIngramLearns@gmail.com>
2025-01-08 07:00:35 -0500

    Update my answers to aggregate functions exercises

---
+++ SQL Deep Dive/Aggregate Functions/myAnswers.sql
@@ -0,0 +1,61 @@
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: Employees
+ */
+-- 
+
+
+-- Question 1: What is the average salary for the company?
+-- Table: Salaries
+
+SELECT AVG(salary)
+FROM public.salaries;
+
+-- Question 2: What year was the youngest person born in the company?
+-- Table: employees
+
+SELECT MAX(birth_date)
+FROM public.employees;
+
+
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: France
+ */
+-- 
+
+-- Question 1: How many towns are there in france?
+-- Table: Towns
+
+SELECT COUNT(id)
+FROM public.towns;
+
+
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: World
+ */
+-- 
+
+-- Question 1: How many official languages are there?
+-- Table: countrylanguage
+
+SELECT COUNT(isofficial)
+FROM countrylanguage
+WHERE isofficial IS TRUE;
+
+-- Question 2: What is the average life expectancy in the world?
+-- Table: country
+
+SELECT AVG(lifeexpectancy)
+FROM country;
+
+-- Question 3: What is the average population for cities in the netherlands?
+-- Table: city
+
+SELECT AVG(population)
+FROM city
+WHERE countrycode = 'NLD';
